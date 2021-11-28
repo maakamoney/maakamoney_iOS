1ceebb6faedeadc3c60db6c6092b8197c776ea9c
Anand Mani R <armani@msystechnologies.com>
2021-11-28 14:57:41 +0530

    Initial Commit

---
+++ MaakanMoney/MaakanMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaakanMoneyApp.swift
+//  MaakanMoney
+//
+//  Created by Anand Mani R on 28/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MaakanMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
